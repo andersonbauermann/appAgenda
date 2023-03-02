51ab9661bee10143ec0d5be182813fd077c0804e
andersonbauermann <anderson.bauermannfeltes@gmail.com>
2023-03-02 07:31:33 -0300

    index e 404
    criada a página index e a página 404 de forma estática

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  secret: 'foo',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
@@ -39,7 +39,6 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(csrf());
-// Nossos próprios middlewares
 app.use(middlewareGlobal);
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
